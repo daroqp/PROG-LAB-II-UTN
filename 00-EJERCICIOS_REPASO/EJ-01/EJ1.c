f3ca9680f2f319a6636fc61a83758c98c079f3b1
Dario <daro.qp@gmail.com>
2021-09-15 05:09:23 -0300

    EJ1 terminado

--- 00-EJERCICIOS_REPASO/EJ-01/EJ1.c
+++ 00-EJERCICIOS_REPASO/EJ-01/EJ1.c
@@ -3,18 +3,27 @@
 int main()
 {
     int nro = 0;
-    int vect[10];
+    int vect[10] = {1,2,3,4,5,6,7,8,9,10};
 
-    for(int i = 0; i < 10; i++){
-        printf("Ingrese un nro: %d\n", i+1);
-        scanf("%d", &nro);
-        vect[i] = nro;
+    for(int i=1; i<10; i++){
+        vect[i] = vect[i] * vect[i-1];
     }
 
-    
     for(int i = 0; i < 10; i++){
-        printf("El numero es %d", &vect[i]);
+        printf("El numero es %d\n", vect[i]);
     }
+
+    int max = 10 - 1;
+    for(int i=0; i< 10/2; i++){
+        int aux = vect[i];
+        vect[i] = vect[max -i];
+        vect[max-i] = aux;
+    }
+    for(int i = 0; i < 10; i++){
+        printf("El numero es %d\n", vect[i]);
+    }
+
+    //Terminado
 
     return 0;
 }
