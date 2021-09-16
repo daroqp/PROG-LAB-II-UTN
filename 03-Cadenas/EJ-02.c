50ae25c626f4c9cec371e7242ae63b80ff7b9422
Dario Quispe <daro.qp@gmail.com>
2021-09-16 20:21:30 -0300

    cadenas Ej1

--- 03-Cadenas/EJ-02.c
+++ 03-Cadenas/EJ-02.c
@@ -3,24 +3,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int largoPalabra(char *palabra){
-    int i = 0;
-    int cant = 0;
-    while (*(palabra+i) != '\0'){
-        cant++;
-        i++;
-    }
-    
-    return cant;
-}
-
 int main(){
 
     char *palabra = (char*)malloc(sizeof(char) * 80);
     printf("Ingrese una palabra: \n");
     scanf("%s", palabra);
 
-    int longPalabra = largoPalabra(palabra);
+    int longPalabra = strlen(palabra);
     bool flag = true;
     for(int i = 0; i < longPalabra/2; i++){
         if(!(*(palabra+i) == *(palabra+(longPalabra-1-i)))){
